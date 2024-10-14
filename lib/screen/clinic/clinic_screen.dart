a7d723be261f5242483e2a0d2e735fc93d20c3f9
the-black-shadow <mehedihasan.cse18@gmail.com>
2024-10-14 20:10:46 +0600

    fixed clinic UI and added doctor remove button

--- lib/screen/clinic/clinic_screen.dart
+++ lib/screen/clinic/clinic_screen.dart
@@ -33,7 +33,9 @@ class _ClinicScreenState extends State<ClinicScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.cyan[100],
       appBar: AppBar(
+        backgroundColor: Colors.cyan[100],
         title: const Text('Clinic'),
         actions: [
           IconButton(
@@ -136,92 +138,120 @@ class ClinicDoctorHomeScreen extends StatelessWidget {
             crossAxisCount: 2, // Two columns per row
             crossAxisSpacing: 10.0,
             mainAxisSpacing: 10.0,
-            childAspectRatio:
-                0.65, // Adjusted this for better image and text layout
+            childAspectRatio: 0.65, // Adjusted for better image and text layout
           ),
           itemCount: doctors.length,
           itemBuilder: (context, index) {
             final doctor = doctors[index];
-            final imageUrl =
-                doctor['imageUrl']; // Get the image URL from Firestore
+            final doctorId = doctor.id; // Get the doctor ID for deletion
+            final imageUrl = doctor['imageUrl'];
             final name = doctor['name'];
             final specialty = doctor['specialty'];
-            final qualification =
-                doctor['qualification']; // Get the qualification from Firestore
+            final qualification = doctor['qualification'];
 
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Image Section
-                  if (imageUrl != null)
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(15.0),
-                        topRight: Radius.circular(15.0),
-                      ),
-                      child: Image.network(
-                        imageUrl,
-                        height: 150, // Fixed height for the image
-                        width: double.infinity,
-                        fit: BoxFit
-                            .cover, // Ensures the image covers the area proportionally
-                        loadingBuilder: (context, child, progress) {
-                          if (progress == null) return child;
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        },
-                        errorBuilder: (context, error, stackTrace) {
-                          return const Icon(
-                            Icons.broken_image,
-                            size: 100,
-                            color: Colors.grey,
-                          );
-                        },
-                      ),
-                    ),
-
-                  // Padding for text content
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          specialty,
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.grey,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          qualification, // Display the qualification
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.blueGrey,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
+            return Stack(
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                ],
-              ),
+                  elevation: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Image Section
+                      if (imageUrl != null)
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(15.0),
+                            topRight: Radius.circular(15.0),
+                          ),
+                          child: Image.network(
+                            imageUrl,
+                            height: 150, // Fixed height for the image
+                            width: double.infinity,
+                            fit: BoxFit
+                                .cover, // Ensures proportional image cover
+                            loadingBuilder: (context, child, progress) {
+                              if (progress == null) return child;
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            },
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Icon(
+                                Icons.broken_image,
+                                size: 100,
+                                color: Colors.grey,
+                              );
+                            },
+                          ),
+                        ),
+
+                      // Padding for text content
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              name,
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              specialty,
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.grey,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              qualification,
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.blueGrey,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Three-dot menu positioned in the top-right corner
+                Positioned(
+                  top: 8.0,
+                  right: 8.0,
+                  child: PopupMenuButton<String>(
+                    onSelected: (value) async {
+                      if (value == 'remove') {
+                        await FirebaseFirestore.instance
+                            .collection('doctors')
+                            .doc(doctorId)
+                            .delete();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Doctor removed')),
+                        );
+                      }
+                    },
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: 'remove',
+                        child: Text('Remove'),
+                      ),
+                    ],
+                    icon: const Icon(Icons.more_vert),
+                  ),
+                ),
+              ],
             );
           },
         );
@@ -252,21 +282,53 @@ class ClinicAppointmentScreen extends StatelessWidget {
             final formattedDate = DateFormat.yMMMd().format(date);
             final patientEmail = appointment['patientEmail'] ?? 'Unknown';
 
-            return ListTile(
-              title: Text('Appointment with Dr. ${appointment['doctorName']}'),
-              subtitle:
-                  Text('Patient Email: $patientEmail\nDate: $formattedDate'),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete, color: Colors.red),
-                onPressed: () async {
-                  await FirebaseFirestore.instance
-                      .collection('appointments')
-                      .doc(appointment.id)
-                      .delete();
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 15.0),
+                title: Text(
+                  'Appointment with Dr. ${appointment['doctorName']}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                subtitle: Text(
+                  'Patient Email: $patientEmail\nDate: $formattedDate',
+                  style: const TextStyle(fontSize: 14),
+                ),
+                leading: const Icon(
+                  Icons.circle_outlined,
+                  color: Colors.blue,
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  onPressed: () async {
+                    await FirebaseFirestore.instance
+                        .collection('appointments')
+                        .doc(appointment.id)
+                        .delete();
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Appointment deleted')),
-                  );
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Appointment deleted')),
+                    );
+                  },
+                ),
+                onTap: () {
+                  // Action when tapped, e.g., navigate to appointment details
                 },
               ),
             );
--- lib/screen/patient/patient_appointment_schedule_screen.dart
+++ lib/screen/patient/patient_appointment_schedule_screen.dart
@@ -3,8 +3,33 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class PatientAppointmentScheduleScreen extends StatelessWidget {
+class PatientAppointmentScheduleScreen extends StatefulWidget {
   const PatientAppointmentScheduleScreen({super.key});
+
+  @override
+  _PatientAppointmentScheduleScreenState createState() =>
+      _PatientAppointmentScheduleScreenState();
+}
+
+class _PatientAppointmentScheduleScreenState
+    extends State<PatientAppointmentScheduleScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 700),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +60,58 @@ class PatientAppointmentScheduleScreen extends StatelessWidget {
             final date = (appointment['date'] as Timestamp).toDate();
             final formattedDate = DateFormat.yMMMd().format(date);
 
-            return ListTile(
-              title: Text('Appointment with ${appointment['doctorName']}'),
-              subtitle: Text('Date: $formattedDate'),
+            // Create an animation for each ListTile
+            final Animation<Offset> slideAnimation = Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(CurvedAnimation(
+              parent: _animationController,
+              curve: Interval(
+                index * 0.1,
+                1.0,
+                curve: Curves.easeOut,
+              ),
+            ));
+
+            // Start the animation
+            _animationController.forward();
+
+            return SlideTransition(
+              position: slideAnimation,
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 15.0),
+                  title: Text(
+                    'Appointment with Dr. ${appointment['doctorName']}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  subtitle: Text('Date: $formattedDate'),
+                  leading: const Icon(
+                    Icons.circle_outlined,
+                    color: Colors.blue,
+                  ),
+                  onTap: () {
+                    // Define action on tap, e.g., open appointment details
+                  },
+                ),
+              ),
             );
           },
         );
